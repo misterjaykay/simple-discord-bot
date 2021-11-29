8d5905f617f77849bbcf448ccc936b9e9dd4a5f8
Jonghyun Kang <misterjaykay@gmail.com>
2021-11-28 22:17:36 -0600

    text edit

--- commands/santa-date.js
+++ commands/santa-date.js
@@ -25,7 +25,7 @@ module.exports = {
     if (fullToday != fullStart) {
         message.channel.send(`오늘은 시작하는 날이 아닙니다. \n시작하는 날은 ${fullStart} 입니다.`);
     } else {
-        message.channel.send("이미 시작되었습니다. !!내상대 로 자기 상대를 확인하시면 됩니다.");
+        message.channel.send("이미 시작되었습니다. !내마니또 로 자기 상대를 확인하시면 됩니다.");
     }
 
   },
