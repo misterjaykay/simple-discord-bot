f5de5152c74f91a42e3105d8f8c2187083c01185
Jonghyun Kang <misterjaykay@gmail.com>
2021-11-29 21:26:17 -0600

    whisp command fix

--- commands/santa-whisper.js
+++ commands/santa-whisper.js
@@ -8,18 +8,22 @@ module.exports = {
     db.Person.find({
       userId: user.id,
     })
-      .then((res) => console.log(res)) 
+      .then((res) => {
+        // console.log(res)
+        const target = res.santaId;
+        let userMsg = "";
+        for (let i = 0; i < args.length; i++) {
+          const element = args[i] + " ";
+          userMsg += element;
+        }
+        if (res.santaId != "") {
+          client.users.fetch(target, false).then((user) => {
+            user.send(`\`\`\`당신의 마니또가 보낸 메세지 입니다.\`\`\`\n ${userMsg}`);
+          });
+        } else {
+          message.send("현재 당신의 마니또는 없습니다.")
+        }
+      }) 
       .catch((err) => console.log(err));
-
-    let userMsg = "";
-    for (let i = 0; i < args.length; i++) {
-      const element = args[i] + " ";
-      userMsg += element;
-    }
-    console.log(userMsg);
-    message.reply(userMsg);
-    client.users.fetch("234086876061892608", false).then((user) => {
-      user.send(`\`\`\`당신의 마니또가 보낸 메세지 입니다.\`\`\`\n ${userMsg}`);
-    });
   },
 };
