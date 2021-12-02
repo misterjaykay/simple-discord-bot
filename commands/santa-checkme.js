358b221ce94f5caa8c257171d539670dc5214ad0
Jonghyun Kang <misterjaykay@gmail.com>
2021-12-01 18:24:15 -0600

    mygift and check myself fix

--- commands/santa-checkme.js
+++ commands/santa-checkme.js
@@ -8,10 +8,27 @@ module.exports = {
   description: "마니또 상대 확인용",
   execute(message, args, mongoose) {
     const { id } = message.author;
-    db.Person.find({
+    db.Person.findOne({
       userId: id,
     })
-      .then((res) => console.log(res, "found"))
+      .then((res) => {
+        db.Person.findOne({
+          userId: res.userId
+        }).then(res => {
+          if (res != null) {
+            message.channel.send(`당신의 마니또는` +
+            `\`\`\`${res.userName}\`\`\`` +
+            "마니또의 소원은\n" +
+            `\`\`\`${(res.santaGift != null) ? res.santaGift : "없습니다"}\`\`\`` +
+            "입니다.")
+            console.log(res.santaGift, "found")
+          } 
+          else {
+            message.channel.send('당신의 마니또는 없습니다.')
+          }
+        })
+        .catch(err => console.log(err));
+      })
       .catch((err) => console.log(err));
   },
 };
--- commands/santa-mygift.js
+++ commands/santa-mygift.js
@@ -1,24 +1,29 @@
 const db = require("../models");
 
 module.exports = {
-    name: "소원",
-    description: "시크릿 산타 & 마니또 내 선물 정보 입력",
-    execute(message, args, client, mongoose) {
-        const { id } = message.author;
-        let sowon = "";
-        for (let i = 0; i < args.length; i++) {
-            const element = args[i] + " ";
-            sowon += element;
-        }
-
-        db.Person.findOneAndUpdate(
-            { userId: id },
-            { $set: { santaGift: sowon} },
-            { new: true }
-        )
-        .then(res => {
-            console.log("Updated Sowon\n", sowon, `\n${res}`);
-        })
-        .catch(err => console.log(err))
+  name: "소원",
+  description: "시크릿 산타 & 마니또 내 선물 정보 입력",
+  execute(message, args, client, mongoose) {
+    const { id } = message.author;
+    let sowon = "";
+    for (let i = 0; i < args.length; i++) {
+      const element = args[i] + " ";
+      sowon += element;
     }
-}
+
+    db.Person.findOneAndUpdate(
+      { userId: id },
+      { $set: { santaGift: sowon } },
+      { new: true }
+    )
+      .then((res) => {
+        client.channels.cache
+          .get(message.channel.id)
+          .messages.fetch(message.id)
+          .then((message) => message.delete());
+        message.channel.send("당신의 소원이 등록되었습니다.");
+        console.log("Updated Sowon\n", sowon, `\n${res}`);
+      })
+      .catch((err) => console.log(err));
+  },
+};
