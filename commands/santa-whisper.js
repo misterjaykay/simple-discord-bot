e6ddadb53d42033b891af24c041cae8f142b872d
Jonghyun Kang <misterjaykay@gmail.com>
2021-12-01 20:54:58 -0600

    added timeout then delete msg.

--- commands/santa-whisper.js
+++ commands/santa-whisper.js
@@ -18,17 +18,28 @@ module.exports = {
           userMsg += element;
         }
         if (res.santaId != null) {
-          client.users.fetch(target, false).then((user) => {
-            user.send(
-              `당신의 마니또가 보낸 메세지 입니다.\`\`\`${userMsg}\`\`\``
-            );
-            console.log("sent message", userMsg);
-          })
-          .catch(err => console.log(err));
+          client.users
+            .fetch(target, false)
+            .then((user) => {
+              user.send(
+                `당신의 마니또가 보낸 메세지 입니다.\`\`\`${userMsg}\`\`\``
+              );
+              console.log("sent message", userMsg);
+            })
+            .catch((err) => console.log(err));
         } else {
           message.channel.send("마니또가 없네요? 왜지?");
         }
       })
       .catch((err) => console.log(err));
+
+    function deleteNotify() {
+      client.channels.cache
+        .get(message.channel.id)
+        .messages.fetch(message.id)
+        .then((message) => message.delete());
+      message.channel.send("당신의 메시지가 마니또에게 전송됐습니다.");
+    }
+    setTimeout(deleteNotify, 2000);
   },
 };
