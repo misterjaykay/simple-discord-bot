c717951a6ef24b1ccdd5ab3bed6a4725f8a89dd0
Jonghyun Kang <misterjaykay@gmail.com>
2021-12-01 20:50:43 -0600

    error fix

--- commands/santa-whisper.js
+++ commands/santa-whisper.js
@@ -5,24 +5,29 @@ module.exports = {
   description: "whisper",
   execute(message, args, client, mongoose) {
     const user = message.author;
-    db.Person.find({
+    db.Person.findOne({
       userId: user.id,
     })
       .then((res) => {
-        console.log(res);
+        // console.log(res);
         const target = res.santaId;
+        // console.log(target);
         let userMsg = "";
         for (let i = 0; i < args.length; i++) {
           const element = args[i] + " ";
           userMsg += element;
         }
-        client.users.fetch(target, false).then((user) => {
-          user.send(
-            `\`\`\`당신의 마니또가 보낸 메세지 입니다.\`\`\`\n ${userMsg}`
-          );
-          console.log("sent message", userMsg);
-        })
-        .catch(err => console.log(err));
+        if (res.santaId != null) {
+          client.users.fetch(target, false).then((user) => {
+            user.send(
+              `당신의 마니또가 보낸 메세지 입니다.\`\`\`${userMsg}\`\`\``
+            );
+            console.log("sent message", userMsg);
+          })
+          .catch(err => console.log(err));
+        } else {
+          message.channel.send("마니또가 없네요? 왜지?");
+        }
       })
       .catch((err) => console.log(err));
   },
