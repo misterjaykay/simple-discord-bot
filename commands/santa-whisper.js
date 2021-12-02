fff96da213c40cb55f21ea292caabbee91e3f246
Jonghyun Kang <misterjaykay@gmail.com>
2021-12-01 20:41:33 -0600

    removed if statement

--- commands/santa-whisper.js
+++ commands/santa-whisper.js
@@ -9,23 +9,19 @@ module.exports = {
       userId: user.id,
     })
       .then((res) => {
-        console.log(res)
+        console.log(res);
         const target = res.santaId;
         let userMsg = "";
         for (let i = 0; i < args.length; i++) {
           const element = args[i] + " ";
           userMsg += element;
         }
-        if (res.santaId != null) {
-          client.users.fetch(target, false).then((user) => {
-            user.send(
-              `\`\`\`당신의 마니또가 보낸 메세지 입니다.\`\`\`\n ${userMsg}`
-            );
-          });
-          console.log('sent message', userMsg);
-        } else {
-          message.reply("현재 당신의 마니또는 없습니다.");
-        }
+        client.users.fetch(target, false).then((user) => {
+          user.send(
+            `\`\`\`당신의 마니또가 보낸 메세지 입니다.\`\`\`\n ${userMsg}`
+          );
+        });
+        console.log("sent message", userMsg);
       })
       .catch((err) => console.log(err));
   },
