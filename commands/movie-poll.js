0b1c084170a67ebf3a0046c6a21547fb206c052d
Jonghyun Kang <misterjaykay@gmail.com>
2021-12-12 05:00:14 -0600

    movie voting fix.

--- commands/movie-poll.js
+++ commands/movie-poll.js
@@ -9,12 +9,12 @@ module.exports = {
     var replymsg = "현재 투표 결과입니다. \n";
     // console.log(username);
     if (!args.length) {
-      message.channel.send("no arg");
+      message.channel.send("투표 하실 번호를 입력해주세요. 예시) 투표 <번호>");
     } else if (args.length > 1) {
-      message.channel.send("too many arg");
+      message.channel.send("번호를 제대로 입력 하지 않았습니다. 제대로 입력해주세요. 예시) 투표 <번호>");
     } else {
       //   console.log(args[0]);
-      message.channel.send("correct");
+    //   message.channel.send("correct");
       db.Poll.findOne({
         pollId: "919541015923294278",
       })
@@ -27,23 +27,24 @@ module.exports = {
             { "choices.id": userpick },
             { $set: { "choices.$.poll": id + 1 } }
           )
-            .then((res) => console.log(res))
+            .then((res) => {
+                db.Poll.findOne({
+                    pollId: "919541015923294278",
+                  })
+                    .then((res) => {
+                    //   console.log("what", res);
+                      for (let i = 0; i < res.choices.length; i++) {
+                          replymsg += `\`\`\`${i + 1}. ${res.choices[i].name} ${res.choices[i].poll}\n\`\`\``;
+                      }
+                      message.reply(replymsg);
+                    //   console.log(replymsg);
+                    })
+                    .catch((err) => console.log(err));
+            })
             .catch((err) => console.log(err));
         })
         .catch((err) => console.log(err));
-      db.Poll.findOne({
-        pollId: "919541015923294278",
-      })
-        .then((res) => {
-        //   console.log("what", res);
 
-          for (let i = 0; i < res.choices.length; i++) {
-              replymsg += `\`\`\`${i + 1}. ${res.choices[i].name} ${res.choices[i].poll}\n\`\`\``;
-          }
-          message.reply(replymsg);
-        //   console.log(replymsg);
-        })
-        .catch((err) => console.log(err));
     }
   },
 };
