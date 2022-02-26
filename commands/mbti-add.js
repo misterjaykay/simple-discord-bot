51ac80b1eadf4386e519582a8c87b56147f95b17
Jonghyun Kang <misterjaykay@gmail.com>
2022-02-25 20:54:12 -0600

    Making changes on checking MBTI letters.

--- commands/mbti-add.js
+++ commands/mbti-add.js
@@ -11,87 +11,84 @@ module.exports = {
       //   );
       // }
       const mbti = args[0].toUpperCase();
-        
-      const charZero = mbti.charAt(0);
-      const charOne = mbti.charAt(1);
-      const charTwo = mbti.charAt(2);
-      const charThree = mbti.charAt(3);
-
       console.log(mbti);
 
-      if (charZero == "I" && "E") {
-        if (charOne == "S" && "N") {
-          if (charTwo == "F" && "T") {
-            if (charThree == "J" && "P") {
-              client.users.fetch(message.author.id, false).then((user) => {
-                const { username, id } = user;
-                const mbtiObj = new db.Birthday({
-                  userId: id,
-                  userName: username,
-                  mbti: mbti,
-                });
-                db.Birthday.findOne({
-                  userId: id,
-                })
-                  .then(res => {
-                    console.log("this is res", res);
-                    if (res == null) {
-                      mbtiObj
-                        .save()
-                        .then((res) => console.log(res))
-                        .catch((err) => console.log(err));
+      const arr = [
+        "ISFJ",
+        "ESFJ",
+        "INFJ",
+        "ENFJ",
+        "ISTJ",
+        "ESTJ",
+        "INTJ",
+        "ENTJ",
+        "INFP",
+        "ENFP",
+        "INTP",
+        "ENTP",
+        "ISFP",
+        "ESFP",
+        "ISTP",
+        "ESTP",
+      ];
 
-                        // Getting role using their role id
-                        // let role = message.guild.roles.cache.get("859036607989153853");
-                      
-                        // Finding Role Using role names.
-                        let role = message.guild.roles.cache.find(r => r.name == mbti);
-                        // let role = message.guild.roles.cache.find();
-                        // let role = guild.roles.fetch('Valorant');
-                        // || await guild.roles.fetch('ROLEID');
-                        console.log('role check', role);
-                        message.member.roles.add(role.id);
-                        message.react('✅');
-                      
-                        // Assigning or Removing their roles using command
-                        // if (args[0] == 'a') {
-                        //     message.member.roles.add(role.id);
-                        //     message.react('✅');
-                        // }
-                        // if (args[0] == 'r') {
-                        //     message.member.roles.remove(role.id);
-                        //     message.react('✅');
-
-                        // var rolex = message.guild.roles.fetch("821592780404817931").then(res => console.log(res.id));                     
-                        // console.log("result", user, rolex);
-                        // user.roles.addRole(rolex);
-                      return message.channel.send(
-                        `등록되었습니다.\n\`\`\` 이름:${username} MBTI:${mbti}\`\`\``
-                      );
-                    } else {
-                      const { userId, userName, birthday, mbti } = res;
-                      return message.channel.send(
-                        `이미 등록된 유저입니다\n\`\`\` 이름:${userName} MBTI:${mbti}\`\`\``
-                      );
-                    }
-                  })
+      if (arr.includes(mbti)) {
+        client.users.fetch(message.author.id, false).then((user) => {
+          const { username, id } = user;
+          const mbtiObj = new db.Birthday({
+            userId: id,
+            userName: username,
+            mbti: mbti,
+          });
+          db.Birthday.findOne({
+            userId: id,
+          })
+            .then((res) => {
+              console.log("this is res", res);
+              if (res == null) {
+                mbtiObj
+                  .save()
+                  .then((res) => console.log(res))
                   .catch((err) => console.log(err));
-              });
-            } else {
-              return message.channel.send(
-                "MBTI가 제대로 입력되지 않았습니다. (Ex. ISFP ENTP)"
-              );
-            }
-          } else {
-            return message.channel.send(
-              "MBTI가 제대로 입력되지 않았습니다. (Ex. ISFP ENTP)"
-            );
-          }
-        } else {
-          return message.channel.send(
-            "MBTI가 제대로 입력되지 않았습니다. (Ex. ISFP ENTP)"
-          );
-        }
+
+                // Getting role using their role id
+                // let role = message.guild.roles.cache.get("859036607989153853");
+
+                // Finding Role Using role names.
+                let role = message.guild.roles.cache.find(
+                  (r) => r.name == mbti
+                );
+                // let role = message.guild.roles.cache.find();
+                // let role = guild.roles.fetch('Valorant');
+                // || await guild.roles.fetch('ROLEID');
+                console.log("role check", role);
+                message.member.roles.add(role.id);
+                message.react("✅");
+
+                // Assigning or Removing their roles using command
+                // if (args[0] == 'a') {
+                //     message.member.roles.add(role.id);
+                //     message.react('✅');
+                // }
+                // if (args[0] == 'r') {
+                //     message.member.roles.remove(role.id);
+                //     message.react('✅');
+
+                // var rolex = message.guild.roles.fetch("821592780404817931").then(res => console.log(res.id));
+                // console.log("result", user, rolex);
+                // user.roles.addRole(rolex);
+                return message.channel.send(
+                  `등록되었습니다.\n\`\`\` 이름:${username} MBTI:${mbti}\`\`\``
+                );
+              } else {
+                const { userId, userName, birthday, mbti } = res;
+                return message.channel.send(
+                  `이미 등록된 유저입니다\n\`\`\` 이름:${userName} MBTI:${mbti}\`\`\``
+                );
+              }
+            })
+            .catch((err) => console.log(err));
+        });
       } else {
         return message.channel.send(
           "MBTI가 제대로 입력되지 않았습니다. (Ex. ISFP ENTP) 2"
@@ -108,87 +105,82 @@ module.exports = {
       // }
       if (message.author.id == admin) {
         const mbti = args[0].toUpperCase();
-        
-        const charZero = mbti.charAt(0);
-        const charOne = mbti.charAt(1);
-        const charTwo = mbti.charAt(2);
-        const charThree = mbti.charAt(3);
-
         console.log(mbti);
-
-        if (charZero == "I" && "E") {
-          if (charOne == "S" && "N") {
-            if (charTwo == "F" && "T") {
-              if (charThree == "J" && "P") {
-                client.users.fetch(args[0], false).then((user) => {
-                  const { username, id } = user;
-                  const mbtiObj = new db.Birthday({
-                    userId: id,
-                    userName: username,
-                    mbti: mbti,
-                  });
-                  db.Birthday.findOne({
-                    userId: message.author.id,
-                  })
-                    .then(res => {
-                      console.log("this is res", res);
-                      if (res == null) {
-                        mbtiObj
-                          .save()
-                          .then((res) => console.log(res))
-                          .catch((err) => console.log(err));
-
-                          // Getting role using their role id
-                          // let role = message.guild.roles.cache.get("859036607989153853");
-                        
-                          // Finding Role Using role names.
-                          let role = message.guild.roles.cache.find(r => r.name == mbti);
-                          // let role = message.guild.roles.cache.find();
-                          // let role = guild.roles.fetch('Valorant');
-                          // || await guild.roles.fetch('ROLEID');
-                          console.log(role);
-                          message.member.roles.add(role.id);
-                          message.react('✅');
-                        
-                          // Assigning or Removing their roles using command
-                          // if (args[0] == 'a') {
-                          //     message.member.roles.add(role.id);
-                          //     message.react('✅');
-                          // }
-                          // if (args[0] == 'r') {
-                          //     message.member.roles.remove(role.id);
-                          //     message.react('✅');
-  
-                          // var rolex = message.guild.roles.fetch("821592780404817931").then(res => console.log(res.id));                     
-                          // console.log("result", user, rolex);
-                          // user.roles.addRole(rolex);
-                        return message.channel.send(
-                          `등록되었습니다.\n\`\`\` 이름:${username} MBTI:${mbti}\`\`\``
-                        );
-                      } else {
-                        const { userId, userName, birthday, mbti } = res;
-                        return message.channel.send(
-                          `이미 등록된 유저입니다\n\`\`\` 이름:${userName} MBTI:${mbti}\`\`\``
-                        );
-                      }
-                    })
+        const arr = [
+          "ISFJ",
+          "ESFJ",
+          "INFJ",
+          "ENFJ",
+          "ISTJ",
+          "ESTJ",
+          "INTJ",
+          "ENTJ",
+          "INFP",
+          "ENFP",
+          "INTP",
+          "ENTP",
+          "ISFP",
+          "ESFP",
+          "ISTP",
+          "ESTP",
+        ];
+        if (arr.includes(mbti)) {
+          client.users.fetch(args[0], false).then((user) => {
+            const { username, id } = user;
+            const mbtiObj = new db.Birthday({
+              userId: id,
+              userName: username,
+              mbti: mbti,
+            });
+            db.Birthday.findOne({
+              userId: message.author.id,
+            })
+              .then((res) => {
+                console.log("this is res", res);
+                if (res == null) {
+                  mbtiObj
+                    .save()
+                    .then((res) => console.log(res))
                     .catch((err) => console.log(err));
-                });
-              } else {
-                return message.channel.send(
-                  "MBTI가 제대로 입력되지 않았습니다. (Ex. ISFP ENTP)"
-                );
-              }
-            } else {
-              return message.channel.send(
-                "MBTI가 제대로 입력되지 않았습니다. (Ex. ISFP ENTP)"
-              );
-            }
-          } else {
-            return message.channel.send(
-              "MBTI가 제대로 입력되지 않았습니다. (Ex. ISFP ENTP)"
-            );
-          }
+
+                  // Getting role using their role id
+                  // let role = message.guild.roles.cache.get("859036607989153853");
+
+                  // Finding Role Using role names.
+                  let role = message.guild.roles.cache.find(
+                    (r) => r.name == mbti
+                  );
+                  // let role = message.guild.roles.cache.find();
+                  // let role = guild.roles.fetch('Valorant');
+                  // || await guild.roles.fetch('ROLEID');
+                  console.log(role);
+                  message.member.roles.add(role.id);
+                  message.react("✅");
+
+                  // Assigning or Removing their roles using command
+                  // if (args[0] == 'a') {
+                  //     message.member.roles.add(role.id);
+                  //     message.react('✅');
+                  // }
+                  // if (args[0] == 'r') {
+                  //     message.member.roles.remove(role.id);
+                  //     message.react('✅');
+
+                  // var rolex = message.guild.roles.fetch("821592780404817931").then(res => console.log(res.id));
+                  // console.log("result", user, rolex);
+                  // user.roles.addRole(rolex);
+                  return message.channel.send(
+                    `등록되었습니다.\n\`\`\` 이름:${username} MBTI:${mbti}\`\`\``
+                  );
+                } else {
+                  const { userId, userName, birthday, mbti } = res;
+                  return message.channel.send(
+                    `이미 등록된 유저입니다\n\`\`\` 이름:${userName} MBTI:${mbti}\`\`\``
+                  );
+                }
+              })
+              .catch((err) => console.log(err));
+          });
         } else {
           return message.channel.send(
             "MBTI가 제대로 입력되지 않았습니다. (Ex. ISFP ENTP) 2"
@@ -199,6 +191,6 @@ module.exports = {
           "당신은 이 명령어를 사용할 권한이 없습니다. (NOT AN ADMIN)"
         );
       }
-    } 
+    }
   },
 };
